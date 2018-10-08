1127cc8afecc3b41c15411d9c5da7c8725479b9a
JuanNeisa <42445731+JuanNeisa@users.noreply.github.com>
2018-10-08 17:04:31 -0500

    Update intento1.py

--- intento1.py
+++ intento1.py
@@ -1,5 +1,13 @@
-archivo = open("prueba1.txt", "r")
-for linea in archivo.readlines():
-    print(linea)
-    
-archivo.close() 
+
+def leer_archivo(direccion):
+	return [x.split(" ") for x in [y.strip("\n") for y in open(direccion).readlines()]][:-2]
+def coor_inicial(direccion, tamaño):
+	return [x.split(",") for x in [y.strip("\n") for y in open(direccion).readlines()]][tamaño:][:-1]
+def coor_final(direccion, tamaño):
+	return [x.split(",") for x in [y.strip("\n") for y in open(direccion).readlines()]][(tamaño + 1):]
+def tamaño_listas(x):
+    return len(x)
+
+print (tamaño_listas(leer_archivo('prueba1.txt')))
+print(coor_inicial('prueba1.txt' , tamaño_listas(leer_archivo('prueba1.txt'))))
+print(coor_final('prueba1.txt', tamaño_listas(leer_archivo('prueba1.txt'))))
