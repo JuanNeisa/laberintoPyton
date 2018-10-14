40a8b8e52a95a00b7c1e881b3d564a12bd0dcf07
JuanNeisa <42445731+JuanNeisa@users.noreply.github.com>
2018-10-13 23:32:50 -0500

    Update intento1.py

--- intento1.py
+++ intento1.py
@@ -1,18 +1,3 @@
-"""archivo = open("prueba1.txt", "r")
-for linea in archivo.readlines():
-    print(linea)
-    
-archivo.close() 
-
-def
-#def mirarLista():
-    #variable= "hola mundo hallo welt"
-   # print("hola mundo hallo welt".split())
-
-
-#mirarLista()
-"""
-
 def cargar_archivo(archivo):
     return [y.split(" ") for y in [x.strip("\n") for x in open(archivo).readlines()]]
 
@@ -25,9 +10,23 @@ def coordenadas_inicio(archivo):
 def coordenadas_fin(archivo):
     return cargar_archivo(archivo)[-1][0].split(",")
 
+def x(lista, numero):
+    return int(lista[numero])
 
-print laberinto("prueba1.txt")
+def c(lab,inicio):
+    if(lab[x(inicio,0)][x(inicio,1)] == '1'):
+        return "Paila"
+    else:
+        return "yeeu"
+        
+    
 
-print coordenadas_inicio("prueba1.txt")
+print (laberinto("prueba1.txt"))
 
-print coordenadas_fin("prueba1.txt")
+print (coordenadas_inicio("prueba1.txt"))
+
+print (coordenadas_fin("prueba1.txt"))
+
+print (c(laberinto("prueba1.txt"), coordenadas_inicio("prueba1.txt")))
+
+
