20d5636daae2e9479dd7836823dcfb1e2d561282
JuanNeisa <42445731+JuanNeisa@users.noreply.github.com>
2018-10-13 23:34:07 -0500

    Update intento1.py

--- intento1.py
+++ intento1.py
@@ -1,13 +1,31 @@
+def cargar_archivo(archivo):
+    return [y.split(" ") for y in [x.strip("\n") for x in open(archivo).readlines()]]
 
-def leer_archivo(direccion):
-	return [x.split(" ") for x in [y.strip("\n") for y in open(direccion).readlines()]][:-2]
-def coor_inicial(direccion, tamaño):
-	return [x.split(",") for x in [y.strip("\n") for y in open(direccion).readlines()]][tamaño:][:-1]
-def coor_final(direccion, tamaño):
-	return [x.split(",") for x in [y.strip("\n") for y in open(direccion).readlines()]][(tamaño + 1):]
-def tamaño_listas(x):
-    return len(x)
+def laberinto(archivo):
+    return cargar_archivo(archivo)[:-2]
 
-print (tamaño_listas(leer_archivo('prueba1.txt')))
-print(coor_inicial('prueba1.txt' , tamaño_listas(leer_archivo('prueba1.txt'))))
-print(coor_final('prueba1.txt', tamaño_listas(leer_archivo('prueba1.txt'))))
+def coordenadas_inicio(archivo):
+    return cargar_archivo(archivo)[-2][0].split(",")
+
+def coordenadas_fin(archivo):
+    return cargar_archivo(archivo)[-1][0].split(",")
+
+def x(lista, numero):
+    return int(lista[numero])
+
+def c(lab,inicio):
+    if(lab[x(inicio,0)][x(inicio,1)] == '1'):
+        return "Paila"
+    else:
+        return "yeeu"
+        
+    
+
+print (laberinto("prueba1.txt"))
+
+print (coordenadas_inicio("prueba1.txt"))
+
+print (coordenadas_fin("prueba1.txt"))
+
+print (c(laberinto("prueba1.txt"), coordenadas_inicio("prueba1.txt")))
+
